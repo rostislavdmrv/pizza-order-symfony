f5937c465b5da85a3002244582ab9f86ed1b609c
Rostislav Demirov <rostyslavz19@gmail.com>
2025-06-07 16:49:45 +0300

    [Internhip-Task] Create Order entity with customer and pizzas.

--- pizza-order/src/Entity/Order.php
+++ pizza-order/src/Entity/Order.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
@@ -17,6 +19,21 @@ class Order
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $comment = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Customer $customer = null;
+
+    /**
+     * @var Collection<int, Pizza>
+     */
+    #[ORM\ManyToMany(targetEntity: Pizza::class)]
+    private Collection $pizzas;
+
+    public function __construct()
+    {
+        $this->pizzas = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +47,42 @@ class Order
     public function setComment(?string $comment): static
     {
         $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): static
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Pizza>
+     */
+    public function getPizzas(): Collection
+    {
+        return $this->pizzas;
+    }
+
+    public function addPizza(Pizza $pizza): static
+    {
+        if (!$this->pizzas->contains($pizza)) {
+            $this->pizzas->add($pizza);
+        }
+
+        return $this;
+    }
+
+    public function removePizza(Pizza $pizza): static
+    {
+        $this->pizzas->removeElement($pizza);
 
         return $this;
     }
