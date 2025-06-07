4aa39eb74f40b358262ceb83fb6a3ea60785aea0
Rostislav Demirov <rostyslavz19@gmail.com>
2025-06-07 19:20:02 +0300

    [Internhip-Task] Add migrations made to the database.

---
+++ pizza-order/migrations/Version20250607152958.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250607152958 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "order" ADD size VARCHAR(255)');
+        $this->addSql("UPDATE \"order\" SET size = 'M' WHERE size IS NULL");
+        $this->addSql('ALTER TABLE "order" ALTER COLUMN size SET NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE "order" DROP size
+        SQL);
+    }
+}
